f81b4aa10481761b04f7c02f3467f9807c6a1014
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-04-07 19:16:41 -0500

    service worker and manifest files

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const FILES_TO_CACHE = [
+    '/public/manifest.webmanifest',
+    'index.html',
+    '/public/js/index.js',
+    '/public/js/db.js',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+    '/public/icons/icon-192x192.png',
+    '/public/icons/icon-512x512.png',
+];
+
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
