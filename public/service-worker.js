12b09af1964401f5a7451ea2b650dd7bb4cbe728
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-04-07 20:18:51 -0500

    service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,7 +13,7 @@ const DATA_CACHE_NAME = 'data-cache-v1';
 
 self.addEventListener('install', function(evt) {
     evt.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
+        caches.open(CACHE_NAME).then((cache) => {
             console.log("Your files were pre-cached successfully!");
             return cache.addAll(FILES_TO_CACHE);
         })
@@ -23,9 +23,9 @@ self.addEventListener('install', function(evt) {
 
 self.addEventListener('activate', function(evt) {
     evt.waitUntil(
-        caches.keys().then(keyList => {
+        caches.keys().then((keyList) => {
             return Promise.all(
-                keyList.map(key => {
+                keyList.map((key) => {
                     if(key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
                         console.log('Removing old cache data', key);
                         return caches.delete(key);
@@ -40,19 +40,19 @@ self.addEventListener('activate', function(evt) {
 self.addEventListener('fetch', function(evt) {
     if(evt.request.url.includes('/api/')) {
         evt.respondWith(
-            caches.open(DATA_CACHE_NAME).then(cache => {
+            caches.open(DATA_CACHE_NAME).then((cache) => {
                 return fetch(evt.request)
-                .then(response => {
+                .then((response) => {
                     if(response.status === 200) {
                         cache.put(evt.request.url, response.clone());
                     }
                     return response;
                 })
-                .catch(err => {
+                .catch(() => {
                     return cache.match(evt.request);
                 });
             })
-            .catch(err => console.log(err))
+            .catch((err) => console.log(err))
         );
         return;
     }
